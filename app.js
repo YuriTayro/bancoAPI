997a86fe3335129d9fe0ee50297a6cc5d940ba14
Yuri Tayro <yuri.tayrocnp.com>
2024-06-07 21:32:24 -0300

    enviando dados da conta

--- app.js
+++ app.js
@@ -2,11 +2,28 @@ import express from "express"
 
 const app = express()
 const port = 3000
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/conta', (req, res) => {
+  res.json({
+    conta_id: 1234,
+    saldo: 18970
+  })
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.post('/conta', (req, res) => {
+
+        const {conta_id, saldo} = req.body
+        if(!conta_id || !saldo){
+            res.status(400).json({message: "Campos conta e saldo são obrigatórios"})
+        }
+    res.json({
+      conta_id: 1234,
+      saldo: 18970
+    }).status(201)
+  })
+
+
+  app.listen(port, '0.0.0.0',() => {
+    console.log(`Example app listening on port ${port}`)
+  })
